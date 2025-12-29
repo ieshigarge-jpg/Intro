aa4df14763fdee1a4117f4ab1e47bad588df5972
ieshigarge-jpg <Ieshi.garge@gmail.com>
2025-12-28 21:42:35 -0500

    Keywords

--- Intro/Intro.py
+++ Intro/Intro.py
@@ -1,6 +1,18 @@
-print("Hello")
-print("Welcome to the world of programming")
-print(4)
-print("Hello Python \n")
-print("Hello",5)
-print("Welcome to ", end="*")
+# # print("Hello")
+# # print("Welcome to the world of programming")
+# # print(4)
+# # print("Hello Python \n")
+# # print("Hello",5)
+# # print("Welcome to ", end="*")
+
+# x=5
+# y= "Joan"
+# print(x)
+# print(y)
+# codingal = "codingal"
+# name = input("enter your name")
+# print(" \nHello", name, "\n welcome to codingal")
+
+import keyword
+print("Python keywords are...\n")
+print(keyword.kwlist)
